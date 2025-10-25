24ada12973ca7dfe31d8c0881a8f33a01e9846a2
DJEUPISNE <oualoumidjeupisne@gmail.com>
2025-10-25 23:13:08 +0000

    fix: correction des alias Sequelize dans les associations

--- models/index.js
+++ models/index.js
@@ -8,26 +8,26 @@ const AuditLog = require('./AuditLog');
 
 // Définir les relations
 User.hasMany(MedicalFile, { foreignKey: 'patientId', as: 'medicalFiles' });
-MedicalFile.belongsTo(User, { foreignKey: 'patientId', as: 'patient' });
-MedicalFile.belongsTo(User, { foreignKey: 'doctorId', as: 'doctor' });
+MedicalFile.belongsTo(User, { foreignKey: 'patientId', as: 'medicalFilePatient' });
+MedicalFile.belongsTo(User, { foreignKey: 'doctorId', as: 'medicalFileDoctor' });
 
 User.hasMany(Appointment, { foreignKey: 'patientId', as: 'patientAppointments' });
 User.hasMany(Appointment, { foreignKey: 'doctorId', as: 'doctorAppointments' });
-Appointment.belongsTo(User, { foreignKey: 'patientId', as: 'patient' });
-Appointment.belongsTo(User, { foreignKey: 'doctorId', as: 'doctor' });
+Appointment.belongsTo(User, { foreignKey: 'patientId', as: 'appointmentPatient' });
+Appointment.belongsTo(User, { foreignKey: 'doctorId', as: 'appointmentDoctor' });
 
 Appointment.hasOne(Payment, { foreignKey: 'appointmentId', as: 'payment' });
-Payment.belongsTo(Appointment, { foreignKey: 'appointmentId', as: 'appointment' });
-Payment.belongsTo(User, { foreignKey: 'patientId', as: 'patient' });
-Payment.belongsTo(User, { foreignKey: 'doctorId', as: 'doctor' });
+Payment.belongsTo(Appointment, { foreignKey: 'appointmentId', as: 'paymentAppointment' });
+Payment.belongsTo(User, { foreignKey: 'patientId', as: 'paymentPatient' });
+Payment.belongsTo(User, { foreignKey: 'doctorId', as: 'paymentDoctor' });
 
 User.hasMany(Notification, { foreignKey: 'userId', as: 'notifications' });
-Notification.belongsTo(User, { foreignKey: 'userId', as: 'user' });
+Notification.belongsTo(User, { foreignKey: 'userId', as: 'notificationUser' });
 
 User.hasMany(Review, { foreignKey: 'patientId', as: 'patientReviews' });
 User.hasMany(Review, { foreignKey: 'doctorId', as: 'doctorReviews' });
-Review.belongsTo(User, { foreignKey: 'patientId', as: 'patient' });
-Review.belongsTo(User, { foreignKey: 'doctorId', as: 'doctor' });
+Review.belongsTo(User, { foreignKey: 'patientId', as: 'reviewPatient' });
+Review.belongsTo(User, { foreignKey: 'doctorId', as: 'reviewDoctor' });
 
 module.exports = {
   User,
