fd8d40178abe060d30895cc551e9fad51d95cf06
Djeupisne <oualoumidjeupisne@gmail.com>
2026-02-14 03:58:45 +0000

    Update adminUsers.js

--- config/adminUsers.js
+++ config/adminUsers.js
@@ -1,29 +1,22 @@
-const bcrypt = require('bcrypt');
-
-// Liste des admins prédéfinis (NE PAS stocker en clair)
 const ADMIN_USERS = [
   {
+    id: 'admin-1',
     email: 'admin@carnetsante.com',
-    // Hash de 'Admin123!' généré avec bcrypt
-    passwordHash: '$2b$10$XwZ5Q7k9Yh3qL5nR8tV2pO1mJ3xY9zK7wL4nR6tV8xZ2pQ5mK9', 
+    // Remplacez ce hash par celui généré par le script
+    passwordHash: '$2a$12$7/qHrWkNkXjgcwHfgm4i0..jjxjr.noa7rEDTbY2aRG7/Gx6dYMGO',
     firstName: 'Super',
     lastName: 'Admin',
     role: 'admin'
   },
   {
+    id: 'admin-2',
     email: 'superadmin@carnetsante.com',
-    // Hash de 'SuperAdmin123!'
-    passwordHash: '$2b$10$YwZ5Q7k9Yh3qL5nR8tV2pO1mJ3xY9zK7wL4nR6tV8xZ2pQ5mK0',
+    // Remplacez ce hash par celui généré par le script
+    passwordHash: '$2a$12$cVt52.Vu3/xPvq1oMH3cjO8a3oIJIWgUJ5KplKFmR0Lri5bfrhMbi',
     firstName: 'Master',
     lastName: 'Admin',
     role: 'admin'
   }
 ];
 
-// Fonction pour générer les hash (à utiliser UNE SEULE fois)
-const generateHash = async (password) => {
-  const salt = await bcrypt.genSalt(10);
-  return await bcrypt.hash(password, salt);
-};
-
-module.exports = { ADMIN_USERS, generateHash };
+module.exports = { ADMIN_USERS };
